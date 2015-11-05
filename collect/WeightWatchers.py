459eb2a23f2a54e9f9dbb19c7104834d58f8e3a5
Ubuntu <ubuntu@ip-172-31-35-71.us-west-2.compute.internal>
2015-11-05 05:20:39 +0000

    final commit before running script overnight

--- collect/WeightWatchers.py
+++ collect/WeightWatchers.py
@@ -8,7 +8,7 @@ class Scraper(object):
     def __init__(self):
         self.search_request = {
             "Page":1,
-            "Count":80, # max seems to be 100
+            "Count":1, # max seems to be 100
             "Country":"US",
             "Type":"Zip", # "State"
             "ZipCode":"19004"
@@ -21,7 +21,7 @@ class Scraper(object):
 
         self.url = 'https://mobile.weightwatchers.com/MeetingsService.svc/FindwithfullDetails'
 
-    def scrape(self, zip_code='19004', count=80):
+    def scrape(self, zip_code='19004', count=1):
         self.search_request['ZipCode'] = zip_code
         self.search_request['Count'] = count
         payload = json.dumps(self.search_request)
@@ -31,10 +31,35 @@ class Scraper(object):
             data=payload,
             headers=self.headers
         )
-
+        
         response = r.json()
         return response
 
 if __name__ == '__main__':
+    import pandas as pd
     scraper = Scraper()
-    print scraper.scrape('11238')
+
+    zip_codes = pd.read_csv('../config/zip_code_database.csv', index_col=0) # NB: Does this convert to integer?
+    standard_zip_codes = [str(e).zfill(5) for e in zip_codes.query(" type == 'STANDARD' ").index] # standard 5 digit zip codes
+    standard_zip_codes_checked = pd.Series(False, index=standard_zip_codes)
+
+    unchecked_zips = standard_zip_codes_checked.replace(True, pd.np.nan).dropna()    
+    #for i in range(10): # for now
+    while len(unchecked_zips) > 0:
+        z = unchecked_zips.index[0] # unchecked_zips[0] # next unchecked
+        initial_n = sum(1 - standard_zip_codes_checked)
+        data = scraper.scrape(zip_code=z, count=100)
+        if data['TotalCount'] > 0:
+            pd.to_pickle(data, '../data/20151105/%s.pkl'%z)
+        checked_zips = [m['Address']['ZipCode'] for m in data['meetingLocations']] + [z] # add back [z] in case it wasn't seen (i.e., no location)
+        #unchecked_zips = [k for k in unchecked_zips if k not in checked_zips] # remove these from unchecked list
+        ok_idx = [k for k in checked_zips if k in standard_zip_codes]
+        unrecognized_zips = [k for k in checked_zips if k not in standard_zip_codes]
+        if len(unrecognized_zips) > 0:
+            print unrecognized_zips
+        standard_zip_codes_checked.ix[ok_idx] = True
+        #print checked_zips
+        unchecked_zips = standard_zip_codes_checked.replace(True, pd.np.nan).dropna()            
+        #assert len(unchecked_zips) == initial_n - len(set(checked_zips)) # Verify that unchecked_zips has 1 fewer member for each zip code checked
+        print '|'.join([str(e) for e in [len(data['meetingLocations']), z, len(unchecked_zips)]])
+        
